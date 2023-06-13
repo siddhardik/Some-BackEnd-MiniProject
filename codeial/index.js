a6fb7d4bc11d2b150d170cc0c26ca0ebdf175828
siddhardik <siddharthabhunia2001@gmail.com>
2023-06-13 19:05:44 +0530

    Codeial Passport Js Authentication Full Complete

--- codeial/index.js
+++ codeial/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 
-const sassMiddleware=require('node-sass-middleware');
+// const sassMiddleware=require('node-sass-middleware');
 
 
 const MongoStore= require('connect-mongo');
